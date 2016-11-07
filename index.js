69b535a3d0445f2794d0656970a1678b242889fe
ValentinLipatov <valentin-ltd@mail.ru>
2016-11-07 18:55:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 var express = require("express");
-var http = require("http");
-var https = require("https");
-
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) 
 {
-	var urlParsed = url.parse(request.url, true);
 	console.log(request.method);
-	console.log(request.headers);
-	console.log(request.url);
-	response.write('hi');
-	response.send('Hello World! ' + urlParsed);
+    console.log(request.headers);
+    console.log(request.url);
+    response.write('hi');
+	response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
