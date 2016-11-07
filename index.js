eaf649b2a43619a65d8920985e20e3d102349365
ValentinLipatov <valentin-ltd@mail.ru>
2016-11-07 19:21:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,7 @@ app.use(express.logger());
 app.get('/', function(request, response) 
 {
 	var urlParsed = url.parse(request.url, true);
-	console.log(urlParsed);
-	response.end(urlParsed.query.message);
-	if(urlParsed.pathname == '/echo' && urlParsed.query.message)
-	{
-		response.end(urlParsed.query.message);
-	}
-	else
-	{
-		response.statusCode = 404;
-		response.end("Page not found");
-	}
+	response.send(urlParsed.query.message);
 });
-
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port, function() {console.log("Listening on " + port);});
