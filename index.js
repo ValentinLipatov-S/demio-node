79b67e3c324ff8a5b8d2ca5d68a065e26b961d51
ValentinLipatov <valentin-ltd@mail.ru>
2016-11-07 19:17:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var express = require("express");
+var url = require("url");
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) 
 {
 	response.send(request.url + " " + request.method);
+	
+	var urlParsed = url.parse(request.url, true);
+	console.log(urlParsed);
+	
+	if(urlParsed.pathname == '/echo' && urlParsed.query.message)
+	{
+		response.end(urlParsed.query.message);
+	}
+	else
+	{
+		response.statusCode = 404;
+		response.end("Page not found");
+	}
 });
 
 var port = process.env.PORT || 5000;
